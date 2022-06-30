c939126a9321a27adecf97cdb85b7f7fa25f1176
Petikeke <majorpeti94@gmail.com>
2022-06-30 21:25:42 +0200

    Email login & SCSS config

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,102 +1,23 @@
-import { useState } from "react";
-
 import {
-  createAuthUserWithEmailAndPassword,
-  createUserDocumentFromAuth
-} from "../../utils/firebase/firebase.utils";
-import { FormInput } from "../form-input/form-input.component";
-
-import "./sign-up-form.styles.scss";
-import Button from "../button/button.component";
-
-const defaultFormFields = {
-  displayName: "",
-  email: "",
-  password: "",
-  confirmPassword: ""
-};
-
-const SignUpFrom = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const { displayName, email, password, confirmPassword } = formFields;
-
-  const resetFormFields = () => {
-    setFormFields(defaultFormFields);
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+  } from '../../utils/firebase/firebase.utils';
+  
+  import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+  
+  const SignIn = () => {
+    const logGoogleUser = async () => {
+      const { user } = await signInWithGooglePopup();
+      const userDocRef = await createUserDocumentFromAuth(user);
+    };
+  
+    return (
+      <div>
+        <h1>Sign In Page</h1>
+        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <SignUpForm />
+      </div>
+    );
   };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (password !== confirmPassword) {
-      alert("passwords do not match");
-      return;
-    }
-    try {
-      const { user } = await createAuthUserWithEmailAndPassword(
-        email,
-        password
-      );
-
-      await createUserDocumentFromAuth(user, { displayName });
-      resetFormFields();
-    } catch (error) {
-      if (error.code === "auth/email-already-in-use") {
-        alert("Cannor create user,email already exist");
-      } else {
-        console.log("user creation ecountered an error", error);
-      }
-    }
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormFields({ ...formFields, [name]: value });
-  };
-
-  return (
-    <div className="sign-up-container">
-      <h2>Don't have an account ? </h2>
-      <span> Sign up with your email and password </span>
-      <form onSubmit={handleSubmit}>
-        <FormInput
-          label="Display Name"
-          type="text"
-          required
-          onChange={handleChange}
-          name="displayName"
-          value={displayName}
-        />
-
-        <FormInput
-          label="Email"
-          type="email"
-          required
-          onChange={handleChange}
-          name="email"
-          value={email}
-        />
-
-        <FormInput
-          label="Password"
-          type="password"
-          required
-          onChange={handleChange}
-          name="password"
-          value={password}
-        />
-
-        <FormInput
-          label="Confirm password"
-          type="password"
-          required
-          onChange={handleChange}
-          name="confirmPassword"
-          value={confirmPassword}
-        />
-        <Button type="submit">Sign up</Button>
-      </form>
-    </div>
-  );
-};
-export default SignUpFrom;
+  
+  export default SignIn;
